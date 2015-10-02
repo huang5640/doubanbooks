71770262efdcf87fa3e032944be3037d29fae644
jason <huang5640@live.com>
2015-10-01 22:18:31 -0700

    First commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,12 @@
 class Book
   include HTTParty
 
-  base_uri 'https://api.douban.com/v2/book/'
-  default_params fields: "id, title, author", q: "isbn"
+  apiKey = '0087b8655dd56b660197293019804232'
+  base_uri 'http://api.douban.com/'
+  default_params apikey: apiKey, alt: 'json'#, fields: "title, author, link",
   format :json
 
   def self.for term
-    get("", query: {query: term})["element"]
+    get("book/subject/isbn/#{term}", {})
   end
 end
