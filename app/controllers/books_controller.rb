88a7c6ff0c699f3b6b61fad309c0ee6dfd03048d
jason <huang5640@live.com>
2015-10-02 23:34:05 -0700

    Add search by ISBN

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
   def index
-    @search_term = params[:looking_for] || "9787535482495"
-    @books = Book.for(@search_term)
+    @search_term = params[:looking_for] || "python"
+    @books = Book.name(@search_term)
+  end
+
+  def isbn
+  	@search_isbn = params[:looking_for] || "9787020042494" #ISBN for 小王子
+  	@book = Book.isbn(@search_isbn)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,17 @@
 class Book
   include HTTParty
 
-  apiKey = '0087b8655dd56b660197293019804232'
-  base_uri 'http://api.douban.com/'
-  default_params apikey: apiKey, alt: 'json'#, fields: "title, author, link",
+  api_key = '0087b8655dd56b660197293019804232'
+  base_uri 'https://api.douban.com/v2/book'
+  default_params apikey: api_key
   format :json
 
-  def self.for term
-    get("book/subject/isbn/#{term}", {})
+  def self.index term
+    get("/search", query: {q: term, fields: "id,title,author"})["books"]
+  end
+
+  def self.isbn term
+  	add = "/isbn/#{term}"
+  	get(add, query: {fields: "id,title,author"})
   end
 end
